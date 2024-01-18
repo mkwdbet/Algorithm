aec21fd51cde935d45832ad931dc32b440850041
mkwdbet <137257577+mkwdbet@users.noreply.github.com>
2024-01-18 13:37:37 +0900

    Update 2024_01_17.py

--- 2024_01_17.py
+++ 2024_01_17.py
@@ -1,11 +1,15 @@
-#1543
-s = "aba"
-doc = "ababababa"
-for i in range(len(doc)):
-    for j in range(len(s)):
-        if doc[i] == s[j]:
-
-#2798
-
-# 질문
-# 파이썬다운할때 path
+doc = input() # 문서 
+word = input() # 찾을 문자열
+word_len = len(word) #문자열의 길이
+i = 0 # doc 인덱스
+cnt = 0 # 문자열이 나온 횟수 = 답
+while (len(doc) >= i):
+    try:
+        if doc[i : i + word_len] == word:
+            i += word_len
+            cnt += 1
+        else:
+            i += 1
+    except:
+        break
+print(cnt)
